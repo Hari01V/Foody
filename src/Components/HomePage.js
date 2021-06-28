76ed133cf84e567df23c713494c5e23ef3ed68be
Admin <hari01venkat@gmail.com>
2021-06-28 21:04:59 +0530

    few changes

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -12,30 +12,30 @@ export default function HomePage(props) {
   return (
     <>
       <Navbar />
-      <div class="home container">
-        <span class="left-slide-btn">
+      <div className="home container">
+        <span className="left-slide-btn">
           <img src="https://img.icons8.com/glyph-neue/64/000000/chevron-left.png" />
         </span>
-        <span class="right-slide-btn">
+        <span className="right-slide-btn">
           <img src="https://img.icons8.com/glyph-neue/64/000000/chevron-right.png" />
         </span>
-        <div class="offers">
+        <div className="offers">
           {offers.map(offer =>
-            <a class="offers-item" href="/">
-              <img class="offers-image" src={offer.img} alt={offer.alt} />
+            <a className="offers-item" href="/">
+              <img className="offers-image" src={offer.img} alt={offer.alt} />
             </a>
           )}
         </div>
       </div>
 
       {/* TOP BRANDS IN SPOTLIGHT */}
-      <div class="top-brands container">
-        <h1 class="top-brands-heading">Top brands in spotlight</h1>
-        <ul class="brands">
+      <div className="top-brands container">
+        <h1 className="top-brands-heading">Top brands in spotlight</h1>
+        <ul className="brands">
           {brands.map(brand =>
-            <a href="/" class="brands-item">
+            <a href="/" className="brands-item">
               <img src={brand.img} alt={brand.alt} />
-              <span class="brand-time">{brand.time}</span>
+              <span className="brand-time">{brand.time}</span>
             </a>
           )}
         </ul>
--- src/Components/Restaurants.js
+++ src/Components/Restaurants.js
@@ -9,40 +9,40 @@ import { restaurants } from '../HelperDatabase';
 export default function Restaurants(props) {
 
   return (
-    <div class="restaurants container">
-      <div class="restaurants-header">
-        <span class="restaurants-heading">161 restaurants</span>
-        <ul class="restaurants-header-list">
-          <li class="restaurants-header-item active">Relevance</li>
-          <li class="restaurants-header-item">Cost for two</li>
-          <li class="restaurants-header-item">Delivery time</li>
-          <li class="restaurants-header-item">Rating</li>
-          <li class="restaurants-header-item restaurants-filter-btn">Filters</li>
+    <div className="restaurants container">
+      <div className="restaurants-header">
+        <span className="restaurants-heading">161 restaurants</span>
+        <ul className="restaurants-header-list">
+          <li className="restaurants-header-item active">Relevance</li>
+          <li className="restaurants-header-item">Cost for two</li>
+          <li className="restaurants-header-item">Delivery time</li>
+          <li className="restaurants-header-item">Rating</li>
+          <li className="restaurants-header-item restaurants-filter-btn">Filters</li>
         </ul>
       </div>
-      <ul class="restaurants-list">
+      <ul className="restaurants-list">
         {
           restaurants.map(restaurant =>
-            <li class="restaurants-item">
+            <li className="restaurants-item">
               <Link className="restaurants-item-link" to={`/restaurants/${restaurant._id}`}>
-                <div class={`restaurants-img-container ${restaurant.isPromoted ? "promoted-tag" : ""}`}>
+                <div className={`restaurants-img-container ${restaurant.isPromoted ? "promoted-tag" : ""}`}>
                   <img src={restaurant.img} alt="" />
                 </div>
-                <div class="restaurants-item-info">
-                  <h3 class="restaurants-name">{restaurant.name}</h3>
-                  <p class="restaurants-desc">{restaurant.desc}</p>
-                  <div class="restaurants-info-detail">
-                    <span class="restaurants-rating">
+                <div className="restaurants-item-info">
+                  <h3 className="restaurants-name">{restaurant.name}</h3>
+                  <p className="restaurants-desc">{restaurant.desc}</p>
+                  <div className="restaurants-info-detail">
+                    <span className="restaurants-rating">
                       <svg width="88" height="84" viewBox="0 0 88 84" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                           d="M42.0979 1.8541C42.6966 0.0114782 45.3034 0.0114802 45.9021 1.8541L54.7767 29.1672C55.0444 29.9912 55.8123 30.5491 56.6788 30.5491H85.3975C87.3349 30.5491 88.1405 33.0284 86.573 34.1672L63.3391 51.0476C62.6382 51.5569 62.3448 52.4596 62.6126 53.2837L71.4872 80.5968C72.0859 82.4394 69.9769 83.9716 68.4095 82.8328L45.1756 65.9524C44.4746 65.4431 43.5254 65.4431 42.8244 65.9524L19.5905 82.8328C18.0231 83.9716 15.9141 82.4394 16.5128 80.5967L25.3874 53.2837C25.6552 52.4596 25.3618 51.5569 24.6609 51.0476L1.42697 34.1672C-0.14046 33.0284 0.665094 30.5491 2.60254 30.5491H31.3212C32.1877 30.5491 32.9556 29.9912 33.2233 29.1672L42.0979 1.8541Z"
                           fill="#fff" />
                       </svg>
                       {restaurant.rating}</span>
-                    <span class="restaurants-time">{restaurant.travelling_time}</span>
-                    <span class="restaurants-rate">{restaurant.rate}</span>
+                    <span className="restaurants-time">{restaurant.travelling_time}</span>
+                    <span className="restaurants-rate">{restaurant.rate}</span>
                   </div>
-                  <div class="restaurants-view">Quick View</div>
+                  <div className="restaurants-view">Quick View</div>
                 </div>
               </Link>
             </li>
