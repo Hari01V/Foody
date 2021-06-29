48e16978607859126784685f0c219db81c1aba64
Admin <hari01venkat@gmail.com>
2021-06-29 19:09:43 +0530

    ordernow completed

--- src/Components/OrderNow.js
+++ src/Components/OrderNow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/OrderNow.css';
 
 import AddIcon from '@material-ui/icons/Add';
@@ -15,18 +15,65 @@ export default function OrderNow(props) {
     menu_title.push(title);
   }
 
+  const scrollToSection = (title, index) => {
+    const section = document.getElementById(`section-${title}-${index}`);
+    section.scrollIntoView({ behavior: 'smooth' });
+
+    document.querySelectorAll(".menu-title").forEach(element => {
+      if (element.id === `${title}-${index}`) {
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+      }
+    })
+
+    var headerOffset = 200;
+    var section_position = section.getBoundingClientRect().top;
+
+    window.scrollBy({
+      top: section_position - headerOffset,
+      behavior: 'smooth'
+    })
+  }
+
+
+  // FOR EFFECTS LIKE SCROLLSPY
+  let observer = null;
+
+  useEffect(() => {
+    observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        const id = entry.target.getAttribute("id");
+        const title_id = id.slice(8);
+        if (document.getElementById(title_id)) {
+          if (entry.intersectionRatio > 0) {
+            document.getElementById(title_id).classList.add("active");
+          } else {
+            document.getElementById(title_id).classList.remove("active");
+          }
+        }
+      })
+    }, { rootMargin: '0px' });
+    document.querySelectorAll(".items-section").forEach(target => {
+      observer.observe(target);
+    });
+  })
+
+
   return (
     <div className="ordernow">
       <ul className="ordernow-menu">
         {menu_title.map((title, index) =>
-          <li className="menu-title active" key={`menu-title-${index}`}>
+          <li id={`${title}-${index}`} className="menu-title" key={`menu-title-${index}`}
+            onClick={() => scrollToSection(title, index)}
+          >
             {title}
           </li>
         )}
       </ul>
       <div className="ordernow-items">
         {Object.keys(menu).map((title, section_index) =>
-          <div className="items-section" key={`section-${title}-${section_index}`}>
+          <div id={`section-${title}-${section_index}`} className="items-section" key={`section-${title}-${section_index}`}>
             <h1 className="section-title">{title}</h1>
             <p className="section-count-items">{menu[title].length} Items</p>
             {menu[title].map((item, index) =>
