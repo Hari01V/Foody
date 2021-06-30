96415bc62ace2096d2d044a971453b5babab587a
Admin <hari01venkat@gmail.com>
2021-06-30 17:54:37 +0530

    homepage-slider not working well!!

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -10,22 +10,49 @@ import { offers, brands } from '../HelperDatabase';
 
 export default function HomePage(props) {
 
+  let count_slide = 0;
+  let slide_offset = 34;
+  const slideLeft = () => {
+    const slider = document.querySelector(".offers");
+    const slide_item = document.querySelector(".offers-item");
+    const slide_size = slide_item.getBoundingClientRect().width + slide_offset;
+    if (count_slide != 0) {
+      count_slide++;
+      slider.style.transform = `translateX(${count_slide * slide_size}px)`;
+    } else {
+      // left slide btn disable
+    }
+  }
+  const slideRight = () => {
+    const slider = document.querySelector(".offers");
+    const slide_item = document.querySelector(".offers-item");
+    const slide_size = slide_item.getBoundingClientRect().width + slide_offset;
+    if (count_slide != 4 - offers.length) {
+      count_slide--;
+      slider.style.transform = `translateX(${count_slide * slide_size}px)`;
+    } else {
+      // right slide btn disable
+    }
+  }
+
   return (
     <>
       <Navbar />
       <div className="home container">
-        <span className="left-slide-btn">
+        <span className="left-slide-btn" onClick={slideLeft}>
           <img src="https://img.icons8.com/glyph-neue/64/000000/chevron-left.png" alt="left-btn" />
         </span>
-        <span className="right-slide-btn">
+        <span className="right-slide-btn" onClick={slideRight}>
           <img src="https://img.icons8.com/glyph-neue/64/000000/chevron-right.png" alt="right-btn" />
         </span>
-        <div className="offers">
-          {offers.map(offer =>
-            <a className="offers-item" href="/">
-              <img className="offers-image" src={offer.img} alt={offer.alt} />
-            </a>
-          )}
+        <div className="offers-slider-container">
+          <div className="offers">
+            {offers.map(offer =>
+              <a className="offers-item" href="/">
+                <img className="offers-image" src={offer.img} alt={offer.alt} />
+              </a>
+            )}
+          </div>
         </div>
       </div>
 
